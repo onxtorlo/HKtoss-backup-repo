9373ac5d8c8d80a5b6c356cc21f4711179d6860f
JMinkyu <mir960609@gmail.com>
2025-06-13 23:10:44 +0900

    recommend.py_issue_edit

--- FastAPI/models/requests.py
+++ FastAPI/models/requests.py
@@ -25,7 +25,7 @@ class SummuryRequest(BaseModel):
 class jsonRequest(BaseModel):
     project_overview: str = Field(..., description="사용자의 아이디어 작성 내용")
     requirements : str = Field(..., description="추가 요구사항 목록")
-    project_summury : str = Field(..., description="프로젝트 요약 내용용")
+    project_summury : str = Field(..., description="프로젝트 요약 내용")
     max_tokens: Optional[int] = Field(8000, ge=1, le=8000, description="생성할 최대 토큰 개수")
     temperature: float = Field(0.3, ge=0.0, le=2.0, description="생성 창의성 정도")
     model: str = Field("ft:gpt-4o-mini-2024-07-18:test::BebIPMSD", description="사용할 모델 이름")
