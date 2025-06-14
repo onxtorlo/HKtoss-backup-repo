a82c47efa40889a03b065143ef62a36541ee89d7
listgreen <listgreen99@naver.com>
2025-06-15 02:55:35 +0900

    fix: requirements.txt 추가 및 워크플로우 수정

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+from fastapi import FastAPI
+from routers import json_summury, requirements, json_ERDAPI, recommendation
+
+app = FastAPI(
+    title="FastAPI LLM Project",
+    description="PJA_ProJect LLM 사용",
+    version="0.0.1"
+)
+
+# 라우터 등록
+app.include_router(requirements.router, prefix="/api/PJA", tags=["요구사항 명세서 생성"])    
+app.include_router(json_summury.router, prefix="/api/PJA", tags=["프로젝트 요약 생성"])
+app.include_router(json_ERDAPI.router, prefix="/api/PJA", tags=["ERD, API 명세서 생성"])
+app.include_router(recommendation.router, prefix="/api/PJA", tags=["프로젝트 진행 추천"])
