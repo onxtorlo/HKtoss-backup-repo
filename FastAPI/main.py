cdea23ee10418b11bcd52d8078eb06f91a682d29
listgreen <listgreen99@naver.com>
2025-06-16 00:53:36 +0900

    test_seong_work-main.py changed 1253

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,5 @@
 # main.py 파일 끝에 주석 한 줄 추가
-# 업데이트 테스트: 2024-06-15 11:56
+# 업데이트 테스트: 2024-06-15 12:51
 from fastapi import FastAPI
 from routers import json_summury, requirements, json_ERDAPI, recommendation
 
